b290adead98d89d01b1666f2a4e9b77cf1976256
Pranay Mohanty <PRM58@pitt.edu>
2018-08-02 13:50:41 -0400

    try

--- aggregates.sql
+++ aggregates.sql
@@ -18,3 +18,21 @@ group by fac.name
 having sum(case when book.memid=0 then book.slots*fac.guestcost
 					     else book.slots*fac.membercost end)<1000;
 
+
+#List the total slots booked per facility per month, part 2:with running sum 
+SELECT facid,EXTRACT(month FROM starttime) AS month,SUM(slots)
+FROM cd.bookings
+WHERE starttime>='2012-01-01' AND starttime<'2013-01-01'
+GROUP BY rollup(facid,month)
+ORDER BY facid,month;
+
+
+
+#List the total hours booked per named facility
+select fac.facid,fac.name,cast(sum(book.slots)*0.5 as decimal(16,2)) as "Total Hours"
+from cd.facilities fac
+inner join cd.bookings book
+on fac.facid=book.facid
+group by fac.facid,fac.name
+order by fac.facid;
+
